e3bcc93fa88daf94a0e6ea4193b1cd36a4bcc81a
GuTory <zmgtothkr@gmail.com>
2024-05-14 09:45:01 +0200

    image tailwind update

--- ng-sandbox-suite/src/app/app.routing.ts
+++ ng-sandbox-suite/src/app/app.routing.ts
@@ -1,3 +1,9 @@
 import {Routes} from "@angular/router";
+import {ImagesComponent} from "./components/images/images.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ImagesComponent
+  }
+];
