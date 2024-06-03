7f5ecfd0abe2d3beee14caeef606e05497b58f6c
Kristof Toth <zmgtothkr@gmail.com>
2024-06-03 12:11:00 +0200

    refactor

--- ng-sandbox-suite/src/app/app.component.ts
+++ ng-sandbox-suite/src/app/app.component.ts
@@ -1,17 +1,30 @@
-import {Component, Signal, computed, inject, signal} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {CommonModule, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
-import {ThemeService} from './utils/services/theme/theme.service';
+import { Component, Signal, computed, inject, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {
+  CommonModule,
+  NgSwitch,
+  NgSwitchCase,
+  NgSwitchDefault,
+} from '@angular/common';
+import { ThemeService } from './utils/services/theme/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, CommonModule]
+  imports: [
+    NgSwitch,
+    NgSwitchDefault,
+    NgSwitchCase,
+    RouterOutlet,
+    CommonModule,
+  ],
 })
 export class AppComponent {
   private themeService = inject(ThemeService);
 
-  public isDarkEnabled: Signal<boolean> = computed(() => this.themeService.isDarkEnabled()) ;
+  public isDarkEnabled: Signal<boolean> = computed(() =>
+    this.themeService.isDarkEnabled()
+  );
 }
--- ng-sandbox-suite/src/app/utils/services/habit/habit.service.ts
+++ ng-sandbox-suite/src/app/utils/services/habit/habit.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { Habit } from '../../models/habit.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HabitService {
-
   habits: Habit = new Map([
     ['2024-05-05', ['Drink water', 'Exercise', 'Read a book']],
     ['2024-05-06', ['Alibi footsal', 'Exercise', 'cook']],
@@ -13,7 +12,7 @@ export class HabitService {
     ['2024-05-24', ['MU WIN', 'Hit chest', 'Read a book']],
   ]);
 
-  constructor() { }
+  constructor() {}
 
   getHabits(): Habit {
     return this.habits;
